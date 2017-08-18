c11e6f436c78838e34001895769c8411d9925df0
sanchagrins <sanchagrins@gmail.com>
2017-08-18 22:19:42 +0900

    Finished imgur util

--- utils/imgur.py
+++ utils/imgur.py
@@ -1,13 +1,16 @@
+import os
 import urllib
 import re
 import requests
 from bs4 import BeautifulSoup
 
 def downloadImage(url, filename):
+    path = './img'
+    makeDir(path)
     response = requests.get(url)
     if response.status_code == 200:
         print('Downloading %s...' % filename)
-        with open(filename, 'wb') as fo:
+        with open(os.path.join(path,filename), 'wb') as fo:
             for chunk in response.iter_content(4096):
                 fo.write(chunk)
 
@@ -25,11 +28,31 @@ def getImageUrl(raw_url):
         
 def getFilename(image_url):
     return re.search(r'(?:[^/][\d\w.]+)+$', image_url).group(0)
-    
-# ############def 
-raw_url = 'https://imgur.com/a/y1rF3'
-image_url = getImageUrl(raw_url)
-print(image_url)
-filename = getFilename(image_url)
-print(filename)
-downloadImage(image_url, filename)
+
+def makeDir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def verifyImgur(url):
+    if 'i.imgur' in url:
+        filename = getFilename(url)
+        print("Downloading {0} | {1}".format(url, filename))
+        downloadImage(url, filename)
+    else:
+        imageUrl = getImageUrl(url)
+        filename = getFilename(imageUrl)
+        print("Downloading {0} | {1}".format(imageUrl, filename))
+        downloadImage(imageUrl, filename)
+
+
+raw_urls = ['https://imgur.com/a/y1rF3','https://imgur.com/gallery/HOVt6','http://i.imgur.com/pwmBfC2.jpg',
+        'http://imgur.com/CCB3LXA']
+
+for url in raw_urls:
+    #image_url = getImageUrl(raw_url)
+    #print(image_url)
+    #filename = getFilename(image_url)
+    #print(filename)
+    #downloadImage(image_url, filename)
+    print("Getting {0}".format(url))
+    verifyImgur(url)
